43d6b8a170aaefd01ed5ae73d952fde9dafa3a26
yoon-jisung <hmsj2380@gamil.com>
2021-04-29 17:03:31 +0900

    [Client] / #10 / fix: css making css layout

--- src/myPages/components/serchUser.js
+++ src/myPages/components/serchUser.js
@@ -1,5 +1,22 @@
 import React, { useState } from 'react';
 
 export default function serchUser() {
-  return <div></div>;
+  return (
+    <section className="follow">
+      <div>이메일로 유저 팔로잉</div>
+      <input type="text" placeholder="유저 이메일"></input>
+      <article>
+        <div>팔로워</div>
+        <ul>
+          <li>유저</li>
+        </ul>
+      </article>
+      <article>
+        <div>팔로잉</div>
+        <ul>
+          <li>유저</li>
+        </ul>
+      </article>
+    </section>
+  );
 }
--- src/myPages/myPage.js
+++ src/myPages/myPage.js
@@ -1,37 +1,17 @@
 import React, { usestate } from 'react';
-import twix from './twix.png';
 import { Link, withRouter, useHistory } from 'react-router-dom';
+import ProFile from './components/ProFile';
+import SerchUser from './components/SerchUser';
+import Header from './components/Header';
+import './MyPages.css';
 
 function MyPage() {
   const history = useHistory();
   return (
     <div>
-      <header>
-        <img src={twix} className="logo"></img>
-        <span onClick={() => history.push('/Waiting')}>나가기</span>
-      </header>
-
-      <section>
-        <buttton>편집</buttton>
-        <div>프로필 사진 변경</div>
-      </section>
-
-      <section>
-        <div>이메일로 유저 팔로잉</div>
-        <input type="text" placeholder="유저 이메일"></input>
-        <article>
-          <div>팔로워</div>
-          <ul>
-            <li>유저</li>
-          </ul>
-        </article>
-        <article>
-          <div>팔로잉</div>
-          <ul>
-            <li>유저</li>
-          </ul>
-        </article>
-      </section>
+      <Header />
+      <ProFile />
+      <SerchUser />
     </div>
   );
 }
