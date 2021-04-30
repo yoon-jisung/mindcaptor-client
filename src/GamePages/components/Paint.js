62adb474bf4635e5e8cbcc24b8002fd031aeab1c
JaeHeon_Kim <jaeheon998795@gmail.com>
2021-04-30 23:45:42 +0900

    [client] / #11 / feature: cursor for each colors

--- src/GamePages/components/Paint.js
+++ src/GamePages/components/Paint.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-export default function Paint({ drawBtn, reset, eraserBtn }) {
+export default function Paint({ reset, eraserBtn }) {
   return (
     <div>
-      <button className="draw" onClick={drawBtn}>
-        그리기
-      </button>
       <button className="reset" onClick={reset}>
         초기화
       </button>
