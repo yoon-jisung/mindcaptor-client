c36a7e686fe4500240c21e359250acb0143c1707
JaeHeon_Kim <jaeheon998795@gmail.com>
2021-04-30 20:57:21 +0900

    [client] / #011 / feature: answer input

--- src/GamePages/InGame.js
+++ src/GamePages/InGame.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Canvas from './components/Canvas3';
 
+import Answer from './components/Answer';
+
 export default function InGame() {
   return (
     <div>
       <Canvas className="canvas" />
+      <Answer />
     </div>
   );
 }
