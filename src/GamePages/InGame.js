247587e2b076a56fd4e033c51e9c837b1ad2afe0
JaeHeon_Kim <jaeheon998795@gmail.com>
2021-04-30 20:51:41 +0900

    [client] / #11 / feature: tiemr

--- src/GamePages/InGame.js
+++ src/GamePages/InGame.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Canvas from './components/Canvas3';
+import Timer from './components/Timer';
 
 export default function InGame() {
   return (
     <div>
+      <Timer />
       <Canvas className="canvas" />
     </div>
   );
