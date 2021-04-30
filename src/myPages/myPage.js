b966f45434458ba1851241a6889fc9b6eb51c861
JaeHeon_Kim <jaeheon998795@gmail.com>
2021-04-30 10:42:39 +0900

    myPage conflicts

--- src/myPages/myPage.js
+++ src/myPages/myPage.js
@@ -1,8 +1,5 @@
 import React, { usestate } from 'react';
-<<<<<<< HEAD
 import logo from '../images/mindcaptor_logo1.png';
-=======
->>>>>>> 195d38f61e5c5e5fc27d684696cfee95e4fe93ec
 import { Link, withRouter, useHistory } from 'react-router-dom';
 import ProFile from './components/ProFile';
 import SerchUser from './components/SerchUser';
@@ -13,7 +10,6 @@ function MyPage() {
   const history = useHistory();
   return (
     <div>
-<<<<<<< HEAD
       <header>
         <img src={logo} className="logo"></img>
         <span onClick={() => history.push('/Waiting')}>나가기</span>
@@ -40,11 +36,6 @@ function MyPage() {
           </ul>
         </article>
       </section>
-=======
-      <Header />
-      <ProFile />
-      <SerchUser />
->>>>>>> 195d38f61e5c5e5fc27d684696cfee95e4fe93ec
     </div>
   );
 }
