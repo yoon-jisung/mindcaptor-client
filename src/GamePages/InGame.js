c4a2a8d56fb2dbe9f542837df25677bdcd030a42
yoon-jisung <hmsj2380@gamil.com>
2021-05-01 17:05:44 +0900

    [Client] / #11 / feature: Make Raedy

--- src/GamePages/InGame.js
+++ src/GamePages/InGame.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Canvas from './components/Canvas3';
 import Timer from './components/Timer';
 import User from './components/User';
 import Answer from './components/Answer';
 
 export default function InGame() {
+  const [IsReady, SetIsReady] = useState(false);
+
+  const CanPlay = () => {
+    setTimeout(() => SetIsReady(true), 3000);
+  };
+
+  const CantPlay = () => {
+    SetIsReady(false);
+  };
+
+  useEffect(() => {
+    console.log(IsReady);
+    CanPlay();
+  });
+
   return (
     <div>
-      <Timer />
-      <Canvas className="canvas" />
-      <Answer />
-      <User />
+      {IsReady ? (
+        <>
+          <Timer />
+          <div className="GameWindow">
+            <Canvas className="canvas" />
+            <Answer />
+            <User />
+          </div>
+        </>
+      ) : (
+        <span className="Ready">
+          <h1>Ready ?</h1>
+          <User />
+        </span>
+      )}
     </div>
   );
 }
