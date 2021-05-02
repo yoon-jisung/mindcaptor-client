279bdba65f18e030d4cc95b2963f62a7284832a3
yoonjisung <71217375+yoon-jisung@users.noreply.github.com>
2021-05-02 20:17:18 +0900

    Revert "Feature #11 result"

--- src/GamePages/components/Answer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Answer() {
-  return (
-    <div className="Answer">
-      <input type="text" className="answer" placeholder="정답을 입력하세요!" />
-    </div>
-  );
-}
