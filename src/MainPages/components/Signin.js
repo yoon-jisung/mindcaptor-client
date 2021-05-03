e950d015e0a7599277484fc731cddb89dcadfed0
jeasong <cjs5241@gmail.com>
2021-05-04 00:22:59 +0900

    feature:login and sociallogin

--- src/MainPages/components/Signin.js
+++ src/MainPages/components/Signin.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import '../../main.css';
 import logo from '../../images/mindcaptor_logo1.png';
 import { useHistory, withRouter, Link } from 'react-router-dom';
+const axios = require('axios');
 
-export default function Signin({ isOpen, close }) {
+export default function Signin({ isOpen, close, loginHandler }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const history = useHistory();
+  const url = `https://accounts.google.com/o/oauth2/auth?client_id=970331179604-upa291p2st8pmj3676qmnm4geurg21cb.apps.googleusercontent.com&redirect_uri=http://localhost:3000&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile email`
+
 
   const emailInputValue = (e) => {
     setEmail(e.target.value);
@@ -16,9 +19,29 @@ export default function Signin({ isOpen, close }) {
     setPassword(e.target.value);
   };
 
-  const handleSignin = () => {
-    history.push('/Waiting');
-  };
+  // const handleSignin = () => {
+  //   history.push('/Waiting');
+  // };
+
+  const loginRequestHandler= ()=> {
+
+    axios
+      .post(
+        "http://localhost:4000/login",
+        { email, password },
+        { headers: { "Content-Type": "application/json" }, withCredentials: true }
+      )
+      .then((res) => {
+        loginHandler(res.data);
+        history.push('/Waiting')
+      })
+      .catch((err) => console.log(err));
+  }
+
+  const socialLoginHandler=() =>{
+    window.location.assign(url)
+  }
+
 
   return (
     <>
@@ -53,12 +76,12 @@ export default function Signin({ isOpen, close }) {
                 placeholder="패스워드"
                 onChange={passwordInputValue}
               />
-              <button className="signin_btn" onClick={handleSignin}>
+              <button className="signin_btn" onClick={loginRequestHandler}>
                 로그인
               </button>
             </div>
             <div className="social_box">
-              <div className="social_btn">구글</div>
+              <div onClick={socialLoginHandler}className="social_btn">구글</div>
               <div className="social_btn">카카오</div>
               <div className="social_btn">네이버</div>
             </div>
