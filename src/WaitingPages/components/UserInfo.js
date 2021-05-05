be906ff22f474272a098da7ef6abcdf5eabf9bfa
yoon-jisung <hmsj2380@gamil.com>
2021-05-05 21:04:26 +0900

    [Client] /#37 / create guest userInfo

--- src/WaitingPages/components/UserInfo.js
+++ src/WaitingPages/components/UserInfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function UserInfo({ accessToken, userInfo }) {
   return (
     <div>
-      <div className="userInfo">{userInfo.nickname}님 !, 안녕하세요</div>
+      <div className="userInfo">{userInfo.nickname}님, 안녕하세요!</div>
     </div>
   );
 }
