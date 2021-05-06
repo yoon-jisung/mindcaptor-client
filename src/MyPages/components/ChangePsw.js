146b45b744fa0025050d509c79f774d038a4309a
JaeHeon_Kim <jaeheon998795@gmail.com>
2021-05-06 12:57:25 +0900

    change psw

--- src/MyPages/components/ChangePsw.js
+++ src/MyPages/components/ChangePsw.js
@@ -10,8 +10,16 @@ export default function ChangePsw() {
         position="bottom center"
         closeOnDocumentClick
       >
-        <input placeholder="현재 비밀번호" className="changePswInput"></input>
-        <input placeholder="새로운 비밀번호" className="changePswInput"></input>
+        <input
+          placeholder="현재 비밀번호"
+          className="changePswOld"
+          className="changePswInput"
+        ></input>
+        <input
+          placeholder="새로운 비밀번호"
+          className="changePswNew"
+          className="changePswInput"
+        ></input>
         <button className="changePswBtn">변경!</button>
       </Popup>{' '}
     </div>
